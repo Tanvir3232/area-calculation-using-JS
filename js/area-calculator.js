9178ccbedf522d53e6db3d865ce4a26dc1b27289
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-02-18 11:43:08 +0600

    added some common function and calculate triangle area

---
+++ js/area-calculator.js
@@ -0,0 +1,42 @@
+function getInputFieldById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldString = inputField.value;
+ 
+    const inputFieldValue  = parseFloat(inputFieldString);
+    return inputFieldValue;
+}
+function getTextElementById(elementId){
+    const element = document.getElementById(elementId);
+    const elementString = element.innerText;
+ 
+    const elementValue  = parseFloat(elementString);
+    return elementValue;
+}
+function area(a,b){
+    return a*b;
+}
+function setOutput(elementId,shapeName,areaValue){
+     const output  = document.getElementById(elementId);
+
+     const li      = document.createElement('li');
+     li.classList.add('my-2','flex','justify-between');
+     const liValue = output.appendChild(li);
+     const btn = document.createElement("BUTTON");
+     const span = document.createElement("span");
+     btn.innerHTML= "Convert m<sup>2</sup>";
+     btn.classList.add('bg-cyan-500', 'text-lg', 'py-2','px-4','rounded-xl');
+     const areaIs  = shapeName;
+     liValue.innerText = areaIs;
+     span.innerHTML = areaValue + "cm<sup>2</sup>";
+    
+     liValue.appendChild(span);
+     liValue.appendChild(btn);
+    
+}
+document.getElementById('calculate-triangle').addEventListener('click',function(){
+     const base   = getInputFieldById('base');
+     const height = getInputFieldById('height');
+
+     const areaTriangle = area(base,height)*0.5;
+     setOutput('output','Triangle',areaTriangle);
+});
